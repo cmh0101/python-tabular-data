281109f1d1a9fac392f6cc805093f56a7cb8cae9
Courtney H <cmh0101@auburn.edu>
2023-03-25 16:15:07 -0500

    I created a script to plot the linear regression for each of the 3 species. Now I will go in to add the function line and docstring.

---
+++ iris.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python3
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+from scipy import stats
+
+
+
+
+
+
+
+dataframe = pd.read_csv("iris.csv")
+
+#versicolor
+
+versicolor = dataframe[dataframe.species == "Iris_versicolor"]
+x = versicolor.petal_length_cm
+y = versicolor.sepal_length_cm
+regression = stats.linregress(x, y)
+slope = regression.slope
+intercept = regression.intercept
+plt.scatter(x, y, label = 'Data')
+plt.plot(x, slope * x + intercept, color = "orange", label = 'Fitted line')
+plt.xlabel("Petal length (cm)")
+plt.ylabel("Sepal length (cm)")
+plt.legend()
+plt.savefig("versicolor_petal_v_sepal_length_regress.png")
+quit()
+
+
+#setosa
+
+setosa = dataframe[dataframe.species == "Iris_setosa"]
+x = setosa.petal_length_cm
+y = setosa.sepal_length_cm
+regression = stats.linregress(x, y)
+slope = regression.slope
+intercept = regression.intercept
+plt.scatter(x, y, label = 'Data')
+plt.plot(x, slope * x + intercept, color = "orange", label = 'Fitted line')
+plt.xlabel("Petal length (cm)")
+plt.ylabel("Sepal length (cm)")
+plt.legend()
+plt.savefig("setosa_petal_v_sepal_length_regress.png")
+quit()
+
+#virginica
+
+virginica = dataframe[dataframe.species == "Iris_virginica"]
+x = virginica.petal_length_cm
+y = virginica.sepal_length_cm
+regression = stats.linregress(x, y)
+slope = regression.slope
+intercept = regression.intercept
+plt.scatter(x, y, label = 'Data')
+plt.plot(x, slope * x + intercept, color = "orange", label = 'Fitted line')
+plt.xlabel("Petal length (cm)")
+plt.ylabel("Sepal length (cm)")
+plt.legend()
+plt.savefig("virginica_petal_v_sepal_length_regress.png")
+quit()
